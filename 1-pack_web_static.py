97022585906c01e5caeca9604e657603267f2128
ngwenyatm <thabsnspaces@gmail.com>
2024-05-19 14:22:35 -0700

    pushed with script

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,15 +5,16 @@ from fabric.api import local
 from os.path import isdir
 
 def do_pack():
-  """
-  This function creates a tar.gz archive of the contents of the web_static directory.
-
-  Returns:
-      str: The path to the created archive file, or None if there was an error.
-  """
-  now = datetime.now()
-  filename = os.path.join("web_static_", "{:%Y%m%d%H%M%S}.tgz".format(now))
-  run("mkdir -p versions", warn=True)
-  archive_path = os.path.join("versions", filename)
-  run(f"tar -cvzf {archive_path} web_static")
-  return archive_path
+    """
+    compress into tgz
+    """
+    f_date = "%Y%m%d%H%M%S"
+    curr_date = datetime.now().strftime(f_date)
+    if not os.path.isdir("versions"):
+        os.makedirs("versions")
+    file = "versions/web_static_{}.tgz".format(curr_date)
+    try:
+        local("tar -cvzf {} web_static".format(file))
+        return file
+    except:
+        return None
