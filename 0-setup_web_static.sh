a85658f683e0db188766203367b433635b47d9f7
ngwenyatm <thabsnspaces@gmail.com>
2024-05-19 10:20:18 -0700

    pushed with script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,12 @@
 #!/usr/bin/env bash
-# Script to set up a web server for deployment
+# sets up web servers for the deployment
 
-# Update and install nginx
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-
-sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-EOF
-
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data
-
-sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
-
-sudo service nginx restart
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
