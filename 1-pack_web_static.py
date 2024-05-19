1e08b4e00a2c3fb32d18c738f3018cb36e435000
ngwenyatm <thabsnspaces@gmail.com>
2024-05-19 14:10:43 -0700

    pushed with script

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,12 +4,16 @@ from datetime import datetime
 from fabric.api import local
 from os.path import isdir
 
-
 def do_pack():
-    curr_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    """ """
 
-    if isdir("versions") is False:
-        local("mkdir versions")
-    file_ = "versions/web_static_{}.tgz".format(curr_date)
-    local("tar -cvzf {} web_static".format(file_))
-    return file_
+    try:
+        curr_date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_ = "versions/web_static_{}.tgz".format(curr_date)
+        local("tar -cvzf {} web_static".format(file_))
+        return file_
+    except:
+        pass
+        return None
