f1bc00ac519c1b89d65fbf06fd097d066bf83ef1
ThabsNspaces <42273431+ngwenyatm@users.noreply.github.com>
2024-06-05 19:27:11 +0200

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -61,3 +61,7 @@ class DBStorage:
             Base.metadata.create_all(self.__engine)
             Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
             self.__session = scooped_session(Session)
+            
+        def close(self):
+            """call remove() method on the private session attribute"""
+            self.__session.remove()
